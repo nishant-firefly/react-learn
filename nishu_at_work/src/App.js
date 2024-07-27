e1cdea0322254bf8fd655bdca4007c2302270b56
nishant-firefly <nishu.saxena@gmail.com>
2024-07-27 13:45:39 -0400

    Despite for function App use arrow function to define same  = ()=>{}

--- nishu_at_work/src/App.js
+++ nishu_at_work/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {// These Are functional components
+const App=  ()=>{ // Changing it to arrow function i.e. after =, will add ()=>{// and here function definition}
   return (
     <div className="App">
       <p>
-        React Cheat Sheet
+        Using arrow function
       </p>
     </div>
   );
 }
-
 export default App;
