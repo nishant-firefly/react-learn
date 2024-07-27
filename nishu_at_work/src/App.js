26a40265c6211144d177f2457cd9a6b9d286d08c
nishant-firefly <nishu.saxena@gmail.com>
2024-07-27 13:56:53 -0400

    name export what is comment

--- nishu_at_work/src/App.js
+++ nishu_at_work/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // New Version no need to import React before we use to have import React from 'react'
 // Componments can be imported or exported from the autofiles. 
 // The purpose to make resusable components without thousands of line of imports. 
+// This one is called name export
 export const App=  ()=>{ // Changing it to arrow function i.e. after =, will add ()=>{// and here function definition}
 // Above because export is in function we need to use import {App} from app 
 // else : export 'default' (imported as 'App') was not found in './App' (possible exports: App)
