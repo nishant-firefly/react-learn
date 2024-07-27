0dc45f6e76371f39d50ec4b204e30f9b1c0bdc68
nishant-firefly <nishu.saxena@gmail.com>
2024-07-27 13:54:57 -0400

    New version reduces imports / If export with function have to use {} in imports

--- nishu_at_work/src/App.js
+++ nishu_at_work/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
-const App=  ()=>{ // Changing it to arrow function i.e. after =, will add ()=>{// and here function definition}
+// New Version no need to import React before we use to have import React from 'react'
+// Componments can be imported or exported from the autofiles. 
+// The purpose to make resusable components without thousands of line of imports. 
+export const App=  ()=>{ // Changing it to arrow function i.e. after =, will add ()=>{// and here function definition}
+// Above because export is in function we need to use import {App} from app 
+// else : export 'default' (imported as 'App') was not found in './App' (possible exports: App)
+  // The fucntion is fundamentally returing a JSX 
   return (
     <div className="App">
       <p>
-        Using arrow function
+        Using arrow function and export function 
       </p>
     </div>
   );
 }
-export default App;
+// This one is using default export and returning JSX inside using index.js 
+// export can be skipped and can write in the function above so commenting below
+// export default App;
